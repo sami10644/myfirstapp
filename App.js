aa40b9194c8a27116127c554b05aca4fbfa45084
sami10644 <61244442+sami10644@users.noreply.github.com>
2021-03-06 02:35:10 +0600

    Update App.js

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import ColorScreen from './src/screens/ColorScreen';
 import SquareScreen from './src/screens/SquareScreen';
 import TextScreen from './src/screens/TextScreen';
 import BoxScreen from './src/screens/BoxScreen';
+
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
