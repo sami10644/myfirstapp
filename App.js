cb8b2b2db5ebecf39b78f295074db49862b60adb
sami10644 <61244442+sami10644@users.noreply.github.com>
2021-03-06 02:37:00 +0600

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { StyleSheet, Text, View } from 'react-native';
+
 import HomeScreen from './src/screens/HomeScreen';
 
 import ComponentsScreen from './src/screens/ComponentsScreen';
