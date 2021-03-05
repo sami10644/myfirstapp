7bc290a69c32a7bb0bbac3a95819827f523d800a
sami10644 <61244442+sami10644@users.noreply.github.com>
2021-03-06 02:35:25 +0600

    Update App.js

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import CounterScreen from './src/screens/CounterScreen';
 import ColorScreen from './src/screens/ColorScreen';
 import SquareScreen from './src/screens/SquareScreen';
 import TextScreen from './src/screens/TextScreen';
+
 import BoxScreen from './src/screens/BoxScreen';
 
 const navigator = createStackNavigator(
