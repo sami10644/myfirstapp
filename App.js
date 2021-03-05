c7685f87e34c39a20d86d9a833fbd153a1a6712c
sami10644 <61244442+sami10644@users.noreply.github.com>
2021-03-06 02:36:13 +0600

    Update App.js

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import HomeScreen from './src/screens/HomeScreen';
 import ComponentsScreen from './src/screens/ComponentsScreen';
 import ListScreen from './src/screens/ListScreen';
 import ImageScreen from './src/screens/ImageScreen';
+
 import CounterScreen from './src/screens/CounterScreen';
 
 import ColorScreen from './src/screens/ColorScreen';
